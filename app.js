ec411e79fdf5a79ccccda8126f3037d69d0e3718
Edgar Romero <edgaromero1987@gmail.com>
2020-04-16 13:51:53 -0700

    add functions on my page

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 
-alert("Hello"); 
-
-var today = new Date ();
-var hourNow = today.getHours ();
-document.write(today);
-
-var city = prompt("Who is your favorite city in EU?");
-if (city == "Boston") {
-    alert("You are correct.");
-} else{
-    alert("Wrong"); 
+function greetings(){
+    alert("Hello"); 
 }
+
+function today(){
+    var today = new Date ();
+    var hourNow = today.getHours ();
+    document.write(today);
+}
+
+function favorite(){
+    var city = prompt("Who is your favorite city in EU?");
+    if (city == "Boston") {
+    alert("You are correct.");
+    } else{
+    alert("Wrong"); 
+    }
+}
+
+greetings();
+today();
+favorite();
 
