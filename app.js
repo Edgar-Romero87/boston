9afa55fcbd9e8b6953aac09c0466178b2f216d0b
Edgar Romero <edgaromero1987@gmail.com>
2020-04-15 21:51:44 -0700

    update page with js

---
+++ app.js
@@ -0,0 +1,14 @@
+
+alert("Hello"); 
+
+var today = new Date ();
+var hourNow = today.getHours ();
+document.write(today);
+
+var city = prompt("Who is your favorite city in EU?");
+if (city == "Boston") {
+    alert("You are correct.");
+} else{
+    alert("Wrong"); 
+}
+
